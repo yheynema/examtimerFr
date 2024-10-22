6b4166248a4404db53a71efcfa1ba8e12431485e
Yanick H <yheynema@gmail.com>
2024-10-22 12:20:58 -0400

    Iteration
    avec le couleurs des heures et le finish clignote (fadeIn/Out)

--- script.js
+++ script.js
@@ -19,14 +19,19 @@ function readConfig() {
 
 function tick() {
 	const now = moment.unix(Math.round(Date.now() / 1000));
-	$("#clock").text(now.format("HH:mm:ss"));
+	$("#clock").text(now.format("HH:mm"));
 
 	const msToEnd = endTime.diff(now);
 	if (msToEnd < 0) {
 		// exam is finished
 		// ...
 		$("#countdown-seconds").text("00");
+		$("#countdown").css("color","red");
+		$("#countdown").fadeOut(500);
+		$("#countdown").fadeIn(500);
 		return;
+	} else {
+		$("#countdown").css("color","white");
 	}
 
 	let duration;
@@ -38,13 +43,19 @@ function tick() {
 		if (msToEnd>0) {
 			$("#end-type").text("avant la fin de l'examen");
 			//Mettre en blanc "time-reading-start"
+			$("#time-reading-start").css("color","white");
 			//Mettre en vert "time-writing-start"
+			$("#time-writing-start").css("color","green");
+			$("#time-end").css("color","red");
 			
 		} else {
 			$("#end-type").text("TERMINÉ");
 			//Mettre en blanc "time-reading-start"
+			$("#time-reading-start").css("color","white");
 			//Mettre en blanc "time-writing-start"
+			$("#time-writing-start").css("color","red");
 			//Mettre en rouge "time-end"
+			$("#time-end").css("color","red");
 		}
 	} else {
 		const msToReading = readingTimeStart.diff(now);
@@ -52,14 +63,29 @@ function tick() {
 			// reading time has started
 			duration = moment.duration(msToWriting);
 			$("#end-type").text("avant la fin de lecture");
+			$("#time-writing-start").css("color","red");
 			//Mettre en vert "time-reading-start"
+			$("#time-reading-start").css("color","green");
 		} else {
 			// waiting for reading time
 			duration = moment.duration(msToReading);
+			$("#time-reading-start").css("color","white");
 			$("#end-type").text("avant la lecture");
 		}
 	}
+	// tenter de contrôler le format d'affichage ... 
+    // Yh 22 oct 2024: tentative de limiter l'affichage des secondes... devra être revu, conjointement avec le html
+/*	if (duration.hours() > 1) {
+		$("#countdown-hours").text(duration.hours());
+		$("#countdown-minutes").text(String(duration.minutes()).padStart(2, "0"));
+		$("#countdown-seconds").text(" ");
 
+	} else {
+		$("#countdown-hours").text(" ");
+		$("#countdown-minutes").text(String(duration.minutes()).padStart(2, "0"));
+		$("#countdown-seconds").text(String(duration.seconds()).padStart(2, "0"));		
+	}
+*/
 		$("#countdown-hours").text(duration.hours());
 		$("#countdown-minutes").text(String(duration.minutes()).padStart(2, "0"));
 		$("#countdown-seconds").text(String(duration.seconds()).padStart(2, "0"));
